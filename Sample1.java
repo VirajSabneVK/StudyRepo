dddb6d5245e7a756d488fb43fc8df90d37615132
Viraj <sabneviraj99@gmail.com>
2024-06-03 17:23:01 -0700

    update

--- Sample1.java
+++ Sample1.java
@@ -4,7 +4,8 @@ class Sample1
 	public static void main(String [] args)
 	
 	{
-		System.out.println("Hello Viraj Sabane The ");
+		System.out.println("Hello Viraj Sabane");
+		System.out.println("First Git File");
 		
 	}
 }
